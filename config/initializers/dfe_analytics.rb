06cb0cb61345dcb9360fcedb317ce756aa290d0c
Slawosz Slawinski <slawosz@gmail.com>
2026-08-27 17:47:00 +0100

    Enable airbyte in DFE-analytics

--- config/initializers/dfe_analytics.rb
+++ config/initializers/dfe_analytics.rb
@@ -14,5 +14,12 @@ DfE::Analytics.configure do |config|
       ENV.fetch("BIGQUERY_DISABLE", disabled_by_default.to_s) != "true"
     end
 
+  config.airbyte_enabled =
+    proc do
+      disabled_by_default = Rails.env.development?
+      ENV.fetch("BIGQUERY_DISABLE", disabled_by_default.to_s) != "true"
+
+  config.airbyte_stream_config_path = 'terraform/aks/workspace_variables/airbyte_stream_config.json'
+
   config.azure_federated_auth = ENV.include? "GOOGLE_CLOUD_CREDENTIALS"
 end
